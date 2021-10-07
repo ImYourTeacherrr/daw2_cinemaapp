b8e5f5615465b00de1a3197068a7a8b79c42eebb
ImYourTeacherrr <apere382@xtec.cat>
2021-10-07 15:52:39 +0200

    Create clsDebug.js

---
+++ com/utils/clsDebug.js
@@ -0,0 +1,17 @@
+class clsDebug{
+
+
+    constructor (){
+        console.log("Objeto de la clase clsDebug creada");
+    }
+
+
+    log (pValue){
+        console.log(pValue);
+    }
+
+    openurl(pValue){
+        window.open(pValue);
+    }
+
+}
