d9fd3685c2903907e43da6042248b73d82bb47aa
ImYourTeacherrr <apere382@xtec.cat>
2021-10-07 15:53:24 +0200

    Initial push

---
+++ com/clsSala.js
@@ -0,0 +1,30 @@
+class clsSala{
+    constructor(pID, pNumerodeasientos){
+        this.number=pID;
+        this.num_asientos=pNumerodeasientos;
+        this.asientos=[];
+        this.containerHTML=document.getElementById("grid_sala");
+        this.CreateAsientos();
+    }
+///////////////////////////////////////////////////////////
+    CreateAsientos(){
+        for (var i=0; i<this.num_asientos; i++){
+            var tA= new clsAsiento(i);
+            this.asientos.push(tA)
+        }
+    }
+///////////////////////////////////////////////////////////
+
+    DrawAsientos(){
+        for (var i=0; i<this.num_asientos; i++){
+            var tA= this.asientos[i];
+            var tCell= tA.Draw()
+            this.containerHTML.appendChild(tCell);
+        }
+    }
+
+    EraseAsientos(){
+        this.containerHTML.innerHTML="";
+    }
+
+}
---
+++ js/main.js
@@ -0,0 +1,15 @@
+var mG
+var d
+
+/*
+d = new clsDebug();
+d.log("que tal");
+d.openurl("https://www.w3schools.com");
+*/
+
+
+window.onload= function(){
+    if (document.readyState=="complete"){
+        mG = new clsCinemaCompany(); 
+    }
+}
