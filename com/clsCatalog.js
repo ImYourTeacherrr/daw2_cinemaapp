9928b294de872096175ac75cd0c23bcd6a7fdd6a
ImYourTeacherrr <apere382@xtec.cat>
2021-10-19 18:03:23 +0200

    t

--- com/clsCatalog.js
+++ com/clsCatalog.js
@@ -25,10 +25,26 @@ class clsCatalog{
     ///////////////////////////////////////////////////////////////////////////////
     Draw(){
 
+
+        var t=`<div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+            <div id="id_carousel" class="carousel-inner">
+            </div>
+        </div>
+        `
+        this.containerHTML.innerHTML=t;
+
+        var inner_carousel=document.getElementById("id_carousel");
+
+
         for (var i=0; i<this._num_peliculas; i++){
             var tF= this._films[i];
             var tCell= tF.Draw()
-            this.containerHTML.appendChild(tCell);
+            inner_carousel.appendChild(tCell);
         }
 
     }
